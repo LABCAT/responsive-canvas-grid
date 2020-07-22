32eca6a9840a284cbfbe76fc45d54d36e8d712b5
LABCAT <shane.watters.nz@gmail.com>
2020-07-22 19:40:11 +1200

    1st attempt at the black square excericise.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,39 @@
 import * as p5 from "p5/lib/p5.min.js";
 
 let sketch = (sketch) => {
+
+    sketch.showGrid = false;
+
     sketch.canvasWidth = window.innerWidth;
     
     sketch.canvasHeight = window.innerHeight;
 
-    sketch.gridColumns = 10;
+    sketch.gridColumns = 30;
 
-    sketch.gridRows = 10;
+    sketch.gridRows = 30;
+
+    sketch.gridColumnWidth = sketch.canvasWidth  / sketch.gridColumns;
+
+    sketch.gridColumnHeight = sketch.canvasHeight / sketch.gridRows;
+    
+    sketch.sizes = {
+        size1: {
+            width: 30,
+            height: 30
+        },
+        size2: {
+            width: 21,
+            height: 21
+        }, 
+        size3: {
+            width: 12,
+            height: 12
+        }, 
+        size4: {
+            width: 6,
+            height: 6
+        }, 
+    }
 
     sketch.setup = () =>{
         sketch.createCanvas(sketch.canvasWidth, sketch.canvasHeight);
@@ -15,20 +41,60 @@ let sketch = (sketch) => {
 
     sketch.draw = () =>{
         //draw grid
+        // sketch.rectMode(sketch.CENTER);
+        // sketch.angleMode(sketch.DEGREES);
         for (var x = 0; x < sketch.canvasWidth; x += sketch.canvasWidth / sketch.gridColumns) {
             for (var y = 0; y < sketch.canvasHeight; y += sketch.canvasHeight / sketch.gridRows) {
-                sketch.stroke(0);
-                sketch.strokeWeight(1);
-                sketch.line(x, 0, x, sketch.canvasHeight);
-                sketch.line(0, y, sketch.canvasWidth, y);
+                if(sketch.showGrid){
+                    sketch.stroke(0, 0, 0);
+                    sketch.strokeWeight(1);
+                    sketch.line(x, 0, x, sketch.canvasHeight);
+                    sketch.line(0, y, sketch.canvasWidth, y);
+                }
+                
+                sketch.blackRect(
+                    13,
+                    2,
+                    sketch.sizes.size3.width,
+                    sketch.sizes.size3.height
+                );
+
+                sketch.blackRect(
+                    13,
+                    16,
+                    sketch.sizes.size3.width,
+                    sketch.sizes.size3.height
+                );
+
+                sketch.blackRect(
+                    -20,
+                    0,
+                    sketch.sizes.size1.width,
+                    sketch.sizes.size1.height
+                );
+
+                sketch.blackRect(
+                    28,
+                    12,
+                    sketch.sizes.size4.width,
+                    sketch.sizes.size4.height
+                );
             }
         }
     }
 
+    sketch.blackRect = (x, y, width, height) => {
+        let calulatedWidth = width * sketch.gridColumnWidth; 
+        let calulatedHeight = height * sketch.gridColumnHeight; 
+        sketch.rect(x * sketch.gridColumnWidth, y * sketch.gridColumnHeight, calulatedWidth, calulatedHeight);
+        sketch.fill(0,0,0)
+    }
+
     sketch.updateCanvasDimensions = () => {
-        console.log('sdfsdfsdf');
         sketch.canvasWidth = window.innerWidth;
         sketch.canvasHeight = window.innerHeight;
+        sketch.gridColumnWidth = sketch.canvasWidth / sketch.gridColumns; 
+        sketch.gridColumnHeight = sketch.canvasHeight / sketch.gridRows; 
         sketch.resizeCanvas(sketch.canvasWidth, sketch.canvasHeight);
         sketch.redraw();
     }
