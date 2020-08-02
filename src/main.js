38043965383d138a67ff3fcb6ac5d27e47643415
LABCAT <shane.watters.nz@gmail.com>
2020-08-02 18:24:42 +1200

    Adding the ability to show/hide the grid and draw rectangles.

--- src/main.js
+++ src/main.js
@@ -3,32 +3,49 @@ import * as p5 from "p5/lib/p5.min.js";
 import './assets/style.css';
 
 let sketch = (sketch) => {
+
+    sketch.showGrid = true;
+
     sketch.canvasWidth = window.innerWidth;
     
     sketch.canvasHeight = window.innerHeight;
 
-    sketch.gridColumns = 10;
+    sketch.gridColumns = 8;
 
-    sketch.gridRows = 10;
+    sketch.gridRows = 8;
+
+    sketch.gridColumnWidth = sketch.canvasWidth / sketch.gridColumns;
+
+    sketch.gridColumnHeight = sketch.canvasHeight / sketch.gridRows;
 
     sketch.setup = () =>{
+        sketch.rectMode(sketch.CENTER);
+        sketch.angleMode(sketch.DEGREES);
         sketch.createCanvas(sketch.canvasWidth, sketch.canvasHeight);
     }
 
     sketch.draw = () =>{
         //draw grid
-        for (var x = 0; x < sketch.canvasWidth; x += sketch.canvasWidth / sketch.gridColumns) {
-            for (var y = 0; y < sketch.canvasHeight; y += sketch.canvasHeight / sketch.gridRows) {
-                sketch.stroke(0);
-                sketch.strokeWeight(1);
-                sketch.line(x, 0, x, sketch.canvasHeight);
-                sketch.line(0, y, sketch.canvasWidth, y);
+        if (sketch.showGrid) {
+            for (var x = 0; x < sketch.canvasWidth; x += sketch.canvasWidth / sketch.gridColumns) {
+                for (var y = 0; y < sketch.canvasHeight; y += sketch.canvasHeight / sketch.gridRows) {
+                    sketch.stroke(0);
+                    sketch.strokeWeight(1);
+                    sketch.line(x, 0, x, sketch.canvasHeight);
+                    sketch.line(0, y, sketch.canvasWidth, y);
+                }
             }
         }
     }
 
+    sketch.gridRect = (x, y, width, height, fill = [0,0,0]) => {
+        let calulatedWidth = width * sketch.gridColumnWidth;
+        let calulatedHeight = height * sketch.gridColumnHeight;
+        sketch.fill(fill[0], fill[1], fill[2]);
+        sketch.rect(x * sketch.gridColumnWidth, y * sketch.gridColumnHeight, calulatedWidth, calulatedHeight);
+    }
+
     sketch.updateCanvasDimensions = () => {
-        console.log('sdfsdfsdf');
         sketch.canvasWidth = window.innerWidth;
         sketch.canvasHeight = window.innerHeight;
         sketch.resizeCanvas(sketch.canvasWidth, sketch.canvasHeight);
